c3ab7933ee38d8cf80774e203717c4cd268715bd
Shalini Mittal <shalini06.mittal@gmail.com>
2023-10-10 12:24:13 +0530

    Spring REST Customer REST FETCH

--- SpringBootRestDemo/src/main/java/com/web/SpringBootRestDemoApplication.java
+++ SpringBootRestDemo/src/main/java/com/web/SpringBootRestDemoApplication.java
@@ -1,13 +1,30 @@
 package com.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.web.entities.Customer;
+import com.web.repo.CustomerRepo;
 
 @SpringBootApplication
 public class SpringBootRestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestDemoApplication.class, args);
+	}
+	
+	@Autowired
+	private CustomerRepo customerRepo;
+	
+	@Bean
+	public void data()
+	{
+		Customer c1 = new Customer("a@a.com", "A", "A", "Mumbai", "India", "1212121212", "aa");
+		Customer c2 = new Customer("b@b.com", "B", "B", "Pune", "India", "2323232323", "bb");
+		customerRepo.save(c1);
+		customerRepo.save(c2);
 	}
 
 }
--- SpringBootRestDemo/src/main/java/com/web/rest/CustomerRestController.java
+++ SpringBootRestDemo/src/main/java/com/web/rest/CustomerRestController.java
@@ -1,11 +1,52 @@
 package com.web.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.web.entities.Customer;
+import com.web.service.CustomerService;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerRestController {
 
 	
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping
+	public List<Customer> getCustomer()
+	{
+		return this.customerService.getAllCustomers(null);
+	}
+	//http://localhost:8080/customers/shalini@gmail.com => path parameter
+	//http://localhost:8080/customers?email=shalini@gmail.com
+	@GetMapping("/{email}")
+	public Customer getCustomer(@PathVariable String email)
+	{
+		return this.customerService.getCustomerByEmail(email);
+	}
+	@PostMapping
+	public List<Customer> addCustomer()
+	{
+		return this.customerService.getAllCustomers(null);
+	}
+	@PutMapping
+	public List<Customer> updateCustomer()
+	{
+		return this.customerService.getAllCustomers(null);
+	}
+	@DeleteMapping
+	public List<Customer> deleteCustomer()
+	{
+		return this.customerService.getAllCustomers(null);
+	}
 }
